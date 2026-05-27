58eb8ca9ae7e9967bde93e02340fbf477ca2e66f
desaiHarshit722 <114604298+desaiHarshit722@users.noreply.github.com>
2026-05-27 12:56:49 +0530

    drizzle and schema setup

---
+++ api/src/db/productsSchema.ts
@@ -0,0 +1,10 @@
+
+import { integer, pgTable, varchar , text, doublePrecision} from "drizzle-orm/pg-core";
+
+export const ProductsTable = pgTable("products", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  name: varchar({ length: 255 }).notNull(),
+  description: text(),
+  image:varchar({length:255}),
+  price:doublePrecision().notNull()
+});
